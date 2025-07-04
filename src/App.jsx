ac0a3d877c6524ad10b02a947a4bf8fa5b3fb601
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-07-04 20:29:00 +0500

    add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ProtectedRoute from './components/ProtectedRoute.jsx'
 import { useAuth } from './context/AuthContext.jsx'
 import {Navigate} from 'react-router-dom'
 import PublicRoute from './components/PublicRoute.jsx'
+import NotFound from './components/not-found.jsx'
 function App() {
   const {user} = useAuth()
   return (
@@ -34,6 +35,7 @@ function App() {
           <Route path='/services' element={<Services/>}/> 
         </Route>
       </Route>
+      <Route path='*' element={<NotFound/>} />
    </Routes>
   )
 }
