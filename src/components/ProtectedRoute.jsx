e3ba01823bb73bd7d7f5d6d3fd554d0114b31672
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-07-04 18:56:56 +0500

    add protected route for user not login  go to protected pages

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import {Outlet,Navigate } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
 const ProtectedRoute = () => {
-    const user = localStorage.getItem('user')
-  return user ? <Outlet/> : <Navigate to={'/login'}/>
+  const {user} = useAuth()
+    
+   
+  return user ? <Outlet /> : <Navigate to={'/login'} />
+  
 }
 
 export default ProtectedRoute
