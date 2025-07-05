f48e75b47159c14393e7df3a617772ebf5ae8512
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-07-06 03:13:03 +0500

    create new add-booking file/component

---
+++ src/pages/add-booking.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+import InvoiceForm from '../components/InvoiceForm'
+const AddBooking = () => {
+  return (
+    <div>
+          <Header />
+          <InvoiceForm/>
+    </div>
+  )
+}
+
+export default AddBooking
