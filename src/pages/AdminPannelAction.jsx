81efb136084f2ce328a7d598615f1fac577e6773
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-07-11 21:32:38 +0500

    .

--- src/pages/AdminPannelAction.jsx
+++ src/pages/AdminPannelAction.jsx
@@ -82,7 +82,7 @@ const AdminPannelAction = () => {
 const handleDeleteContainer = async (id) => {
   const confirmDelete = window.confirm(
     `Are you sure you want to delete this ${actionType === 'branchAction' ? 'branch' : 'city'}?`
-  );
+  )
   if (!confirmDelete) return;
 
   try {
