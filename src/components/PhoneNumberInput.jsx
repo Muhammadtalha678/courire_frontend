bda93b8afa0ffc053e8bacff77e5080a3e272493
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-07-16 10:40:34 +0500

    create a phone number component

---
+++ src/components/PhoneNumberInput.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { PhoneInput } from 'react-international-phone';
+import 'react-international-phone/style.css';
+
+const PhoneNumberInput = ({value,handleChange,name}) => {
+  return (
+    <PhoneInput
+      name={name}
+        defaultCountry="sa"
+        value={value}
+      onChange={(phone) => handleChange({ target: { name, value: phone } })}
+      //  onCountryChange={(country) => setCountry(name, country.dialCode)}
+      inputStyle={{width:'100%'}}
+        className='w-full border'
+    />
+  )
+}
+
+export default PhoneNumberInput
